e2462b9386c09e1bd88b031ff94766d2cde39ff1
rianmanhente <rian.valeente@gmail.com>
2023-12-26 14:21:35 -0300

    subindo site

---
+++ home/main.js
@@ -0,0 +1,48 @@
+const menuIcon = document.getElementById('menuIcon')
+const segundaUl = document.querySelector('.segundaUl')
+const primeiraUl = document.querySelector('.primeiraUl')
+
+menuIcon.onclick = () => {
+    segundaUl.classList.toggle('open')
+    primeiraUl.classList.toggle('open')
+    menuIcon.classList.toggle('bx-x')
+
+}
+
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+
+function hideSlider() {
+  slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider() {
+  slider[currentSlide].classList.add('on')
+}
+
+function nextSlider() {
+  hideSlider()
+  if(currentSlide === slider.length -1) {
+    currentSlide = 0
+  } else {
+    currentSlide++
+  }
+  showSlider()
+}
+
+function prevSlider() {
+  hideSlider()
+  if(currentSlide === 0) {
+    currentSlide = slider.length -1
+  } else {
+    currentSlide--
+  }
+  showSlider()
+}
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider)
+
